f4e5154d4e6a277606c1a9fb9728e2415c045f7a
Dexter-freak <F20212792@pilani.bits-pilani.ac.in>
2024-01-23 23:07:27 +0530

    fix2

--- src/pages/laundro_items.js
+++ src/pages/laundro_items.js
@@ -802,8 +802,8 @@ function Signings({
 
   function getComparator(order, orderBy) {
     return order === "desc"
-      ? (a, b) => descendingComparator(a, b, orderBy)
-      : (a, b) => -descendingComparator(a, b, orderBy);
+      ? (a, b) => -descendingComparator(a, b, orderBy)
+      : (a, b) => descendingComparator(a, b, orderBy);
   }
 
   function stableSort(array, comparator) {
